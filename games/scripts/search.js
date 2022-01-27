801f472b3d0ea7027d95f0aedada5a759aee11c8
KAJAL GANORKAR <kajalganorkar97@gmail.com>
2022-01-27 15:10:57 +0530

    search part

---
+++ games/scripts/search.js
@@ -0,0 +1,25 @@
+let data = JSON.parse(localStorage.getItem("GameData"));
+console.log('data', data);
+
+async function list() {
+    let res = await fetch(`https://api.rawg.io/api/games/${data}?key=cab653694f20430d9700315187126e48`);
+    let dat = await res.json();
+    // console.log('data', dat);
+    displayGame(dat)
+    console.log('results', dat);
+   
+
+}
+list();
+let displayGame = (data) => {
+    var path = document.querySelector("#searchd");
+    
+        var div = document.createElement("div");
+        var title = document.createElement("h2");
+        title.textContent = data.name;
+        var image = document.createElement("img");
+        image.src = data.background_image;
+        div.append(title, image);
+        path.append(div);
+         
+}
