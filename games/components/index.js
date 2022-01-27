ec6118b4fa383f13702df279b4d3ea881bd888ac
MadhanM282 <96064381+MadhanM282@users.noreply.github.com>
2022-01-27 23:41:25 +0530

    addDate

--- games/components/index.js
+++ games/components/index.js
@@ -17,16 +17,23 @@ async function Games(url) {
 
 let displayGame = (data,location) => {
     // var path = document.querySelector("#grid");
-    data.map(({ name, background_image }) => {
+    data.map(({ name, background_image,released }) => {
         var div = document.createElement("div");
+        div.setAttribute("class","bigdiv")
         var title = document.createElement("h1");
         title.textContent = name;
         var image = document.createElement("img");
         image.src = background_image;
 
         let moreDiv = document.createElement("div");
+        // moreDiv.style.display = "none"
+        moreDiv.setAttribute("class","more")
 
-        div.append(image,name );
+        let date = document.createElement("h2")
+        date.innerHTML = released;
+
+        moreDiv.append(date)
+        div.append(image,name,moreDiv);
         location.append(div);
     })
 }
