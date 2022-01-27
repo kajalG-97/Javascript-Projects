e74b6e7c26e3c26825ece3ac981201c4f5030cce
MadhanM282 <96064381+MadhanM282@users.noreply.github.com>
2022-01-27 22:30:57 +0530

    dateschange

--- games/scripts/search.js
+++ games/scripts/search.js
@@ -2,7 +2,7 @@ let data = JSON.parse(localStorage.getItem("GameData"));
 console.log('data', data);
 
 async function list() {
-    let res = await fetch(`https://api.rawg.io/api/games/${data}?key=cab653694f20430d9700315187126e48`);
+    let res = await fetch(`https://api.rawg.io/api/games/${data}?key=cab653694f20430d9700315187126e48&dates=2019-09-01,2022-01-10&platforms=18,1,7`);
     let dat = await res.json();
     // console.log('data', dat);
     displayGame(dat)
